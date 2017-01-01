531d770b13abc48c5c1fd9833eee797d5fdd3cd3
Ashton Cross <ashtoncrossdude@gmail.com>
2017-01-01 12:47:29 -0700

    Hello World! (seventienth time)

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloWorld;
+// the fiftieth time i've said hello world.
+
+public class HelloWorld {
+	public static void main (String[] arguments){
+		System.out.println("Hello world!");
+		// make that sixteen...
+	}
+}
